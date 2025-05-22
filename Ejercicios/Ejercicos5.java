ad95859ef68dc7ef87271227dca81f22743956ac
buty-cague-de-richa <202400893i@utea.edu.pe>
2025-05-22 11:20:44 -0500

    ejercicios

---
+++ Ejercicios/Ejercicos5.java
@@ -0,0 +1,26 @@
+package Ejercicios;
+
+public class Ejercicos5 {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------------------------------------");
+        System.out.println("│   hora      │   LUNES     │    MARTES     │    MIERCOLES │   JUEVES    │    VIERNES │ ");
+        System.out.println("│-------------│-------------│---------------│--------------│---------------------------  ");
+        System.out.println("│06:20 - 08:00│             │  CÁLCULO I    │              │             │            │  ");
+        System.out.println("│08:00 - 08:50│Algoritmos I │               │              │             │            │  ");
+        System.out.println("--------------------------------------------------------------------------------------    ");
+        System.out.println("│08:50 - 09:40│             │ Ing.Sistemas  │              │ Ing.Sistemas│            │  ");
+        System.out.println("│09:40 - 10:30│             │               │              │             │            │     ");
+        System.out.println("--------------------------------------------------------------------------------------");
+        System.out.println("│10:30 - 12:10│             │Ing.Operaciones│              │             │AlgoritmosI │   ");
+        System.out.println("│12:10 - 14:40│             │               │              │             │            │       ");
+        System.out.println("--------------------------------------------------------------------------------------");
+        System.out.println("│14:40 - 16:20│             │               │              │ Estadística │            │     ");
+        System.out.println("│16:20 - 17:10│             │               │              │             │            │         ");
+        System.out.println("---------------------------------------------------------------------------------------");
+        System.out.println("│17:10 - 19:40│CÁLCULO I    │               │              │             │            │");
+        System.out.println("│19:40 - 20:30│             │               │              │             │            │        ");
+        System.out.println("---------------------------------------------------------------------------------------");
+        System.out.println("│20:30 - 22:10│             │               │Mat.Discreta  │             │            │           ");
+        System.out.println("---------------------------------------------------------------------------------------");  
+}
+}
