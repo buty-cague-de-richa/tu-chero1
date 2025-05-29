57bc5c041d1750023f4b78a1e148a8d6e16764cd
buty-cague-de-richa <202400893i@utea.edu.pe>
2025-05-29 10:37:43 -0500

    Asignaciones

---
+++ Asignaciones.java
@@ -0,0 +1,19 @@
+public class Asignaciones {
+    public static void main(String[] args) {
+        int x =2;
+        int y =9;
+        
+        int suma = x + y;
+        System.out.println("la suma es:  " +suma);
+
+        int resta = x - y;
+        System.out.println("la resta es: " +resta);
+
+        int multiplicacion = x * y;
+        System.out.println("la mutiplicacion es: " +multiplicacion);
+
+        double division =  (double)x / (double) y;
+        System.out.println("la division es: " +division);
+        
+    }
+}
