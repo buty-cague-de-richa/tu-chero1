6c8d06730ddce10ffccf0cf9c4cae80bd81df42c
buty-cague-de-richa <202400893i@utea.edu.pe>
2025-06-02 08:42:32 -0500

    Leenumeros

---
+++ LeeNumeros.java
@@ -0,0 +1,19 @@
+public class LeeNumeros {
+    public static void main(String[] args) {
+
+    String linea;
+    System.out.println("ingresa un numero entero");
+    linea =System.console().readLine();
+    
+int numero1;
+numero1= Integer.parseInt(linea);
+ System.out.println("ingresa otro numero: ");
+  linea= System.console().readLine();
+
+  int numero2;
+
+  numero2 = Integer.parseInt(linea);
+   
+System.out.println("la suma de ambos numeros es: " + (numero1 + numero2));
+    }
+}
