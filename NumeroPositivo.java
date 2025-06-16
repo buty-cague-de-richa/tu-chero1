f920f9a1fc095fc5cfe2fd0e79a72870b849076c
buty-cague-de-richa <202400893i@utea.edu.pe>
2025-06-16 08:54:04 -0500

    Numeropositivo

---
+++ NumeroPositivo.java
@@ -0,0 +1,24 @@
+public class NumeroPositivo {
+    public static void main(String[] args) {
+        int numero;
+        System.out.println("por favor, ingresa un numero entero");
+        numero=Integer.parseInt(System.console().readLine());
+        if(numero>0){
+            System.out.println("el numero ingresado es positivo");
+        
+            }else{
+                System.err.println("el numero ingresado es negativo");
+                /* Operadores de comparación
+        
+         * ==   igual
+         * !=   distinto 
+         * <    menor que
+         * >    mayor que
+         * <=   menor o igual que
+         * >=   mayor o igual que
+         */
+    }
+        
+    }
+
+  }
