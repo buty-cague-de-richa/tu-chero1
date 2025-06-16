1088c17548f084721636695238d6e02457f33f2e
buty-cague-de-richa <202400893i@utea.edu.pe>
2025-06-16 08:29:33 -0500

    Comparacion

---
+++ ComparacionCadena.java
@@ -0,0 +1,17 @@
+public class ComparacionCadena {
+    public static void main(String[] args) {
+        String mifruta;
+        System.out.println("Ingrese la fruta de su preferencia");
+        mifruta=System.console().readLine();
+
+        if ("manzana".equals(mifruta)) {
+            System.out.println("iaguales");
+
+            
+        } else {
+            System.out.println("distindas");
+        }
+        
+    }
+    
+}
